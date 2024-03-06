fbc47da7043a2fb1386352a5255d52b29a0ff20c
Ramon Fossa Chiesi <104222191+RamonFossa@users.noreply.github.com>
2024-03-06 19:15:51 +0000

    Finaliza exercicio 10

--- ramon/exercicios/10.py
+++ ramon/exercicios/10.py
@@ -5,8 +5,11 @@ grades = [9, 8.5, 0.5, 10, 1, 4, 5, 6, 6.5, 3]
 def calculate_average(sum, grades_length):
     return (sum / grades_length)
 
-def calcuate_standard_deviation(sum, grades_length, grade, average):
-    return math.sqrt((1/(grades_length-1)) * (sum) * ((grade - average) ** 2))
+def calculate_standard_deviation(grades_length, standard_deviation_sum):
+    return math.sqrt((1/(grades_length-1)) * standard_deviation_sum)
+
+def calculate_standard_deviation_element(grade, average):
+    return (grade - average) ** 2
 
 sum = 0
 grades_length = len(grades)
@@ -15,11 +18,14 @@ for mGrade in grades: sum += mGrade
 
 average = calculate_average(sum, grades_length)
 
-standard_deviation_list = []
+standard_deviation_sum = 0
 
-for mGrade in grades: standard_deviation_list.append(calcuate_standard_deviation(sum, grades_length, mGrade, average))
+for mGrade in grades: standard_deviation_sum += calculate_standard_deviation_element(mGrade, average)
+
+
+standard_deviation = calculate_standard_deviation(grades_length, standard_deviation_sum)
 
 print('AVERAGE:')
 print(average)
-print('STANDART DEVIATION:')
-print(standard_deviation_list)
+print('STANDARD DEVIATION:')
+print(standard_deviation)
